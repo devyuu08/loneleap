fbf48354c2832629516bde3fbaecbfa4b0413955
Yujin Jung <devyuu08@gmail.com>
2025-04-09 21:02:54 +0900

    fix: 일정 상세페이지 돌아가기 버튼 경로 오류 수정(navigate(/reviews)

--- src/pages/Reviews/Detail.jsx
+++ src/pages/Reviews/Detail.jsx
@@ -30,7 +30,7 @@ export default function ReviewDetailPage() {
       <header>
         <div className="mb-2">
           <button
-            onClick={() => navigate("/itinerary")}
+            onClick={() => navigate("/reviews")}
             className="text-sm text-gray-500 mb-2 hover:underline "
           >
             ← 목록으로 돌아가기
