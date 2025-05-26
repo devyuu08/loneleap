775b179ace5cb2a32fcb8d49264c64c0ac7aec30
Yujin Jung <devyuu08@gmail.com>
2025-05-26 18:55:01 +0900

    refactor: 일정 상세 페이지를 Container/Presentational 패턴으로 분리

---
+++ src/containers/itinerary/ItineraryDetailContainer.jsx
@@ -0,0 +1,32 @@
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { useItineraryDetail } from "hooks/itinerary/useItineraryDetail";
+import { useDeleteItinerary } from "hooks/itinerary/useDeleteItinerary";
+
+import LoadingSpinner from "components/common/LoadingSpinner";
+import NotFoundMessage from "components/common/NotFoundMessage";
+import ItineraryDetail from "components/itinerary/ItineraryDetail";
+
+export default function ItineraryDetailContainer() {
+  const { id } = useParams();
+  const currentUser = useSelector((state) => state.user);
+  const { data, isLoading, isError } = useItineraryDetail(id);
+  const { mutate, isPending } = useDeleteItinerary();
+
+  if (isLoading || currentUser.isLoading) return <LoadingSpinner />;
+  if (isError || !data)
+    return <NotFoundMessage message="일정을 찾을 수 없습니다." />;
+
+  const isOwner = currentUser?.user?.uid === data.userId;
+
+  return (
+    <ItineraryDetail
+      itineraryId={id}
+      itinerary={data}
+      isOwner={isOwner}
+      onDelete={mutate}
+      isDeletePending={isPending}
+    />
+  );
+}
