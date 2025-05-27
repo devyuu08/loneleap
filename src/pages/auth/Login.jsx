d742550974efa004864ed2cc4e32edebfea1f91e
Yujin Jung <devyuu08@gmail.com>
2025-05-27 10:55:11 +0900

    fix: 로그인 페이지의 헤더 간격 문제 해결 (불필요한 pt-12 제거)

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,9 +1,5 @@
 import LoginFormContainer from "containers/auth/LoginFormContainer";
 
 export default function Login() {
-  return (
-    <div className="pt-12">
-      <LoginFormContainer />
-    </div>
-  );
+  return <LoginFormContainer />;
 }
