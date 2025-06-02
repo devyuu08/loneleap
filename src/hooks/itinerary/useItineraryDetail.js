4b28d6cb37ccdf894f33efbacbe9fac53685e116
Yujin Jung <devyuu08@gmail.com>
2025-06-02 20:26:39 +0900

    perf: 일정 상세 쿼리에 staleTime 및 에러 핸들러 추가

--- src/hooks/itinerary/useItineraryDetail.js
+++ src/hooks/itinerary/useItineraryDetail.js
@@ -6,6 +6,8 @@ export const useItineraryDetail = (id) => {
   return useQuery({
     queryKey: QUERY_KEYS.ITINERARY_DETAIL(id),
     queryFn: () => fetchItineraryById(id),
+    staleTime: 3 * 60 * 1000,
     enabled: !!id, // id가 존재할 때만 fetch
+    onError: (err) => console.error("상세 일정 불러오기 실패:", err),
   });
 };
