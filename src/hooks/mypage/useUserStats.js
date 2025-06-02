ab2f4b86e7f7cdddd15083f497a622a3c9bbc3ae
Yujin Jung <devyuu08@gmail.com>
2025-06-02 21:53:02 +0900

    perf: useUserStats 훅 서버 요청 최적화

--- src/hooks/mypage/useUserStats.js
+++ src/hooks/mypage/useUserStats.js
@@ -2,12 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchUserStats } from "@/services/user/fetchUserStats";
 import { QUERY_KEYS } from "@/constants/queryKeys";
 
-export const useUserStats = (uid) => {
+export const useUserStats = (uid, options = {}) => {
   return useQuery({
     queryKey: QUERY_KEYS.USER_STATS(uid),
     queryFn: () => fetchUserStats(uid),
-    enabled: !!uid,
-    staleTime: 0,
-    refetchOnWindowFocus: true,
+    enabled: !!uid && options.enabled !== false,
+    staleTime: 60 * 1000, // 1분 동안 fresh 상태 유지
+    cacheTime: 5 * 60 * 1000, // 5분 간 메모리 캐시 유지
+    refetchOnWindowFocus: false,
+    ...options,
   });
 };
