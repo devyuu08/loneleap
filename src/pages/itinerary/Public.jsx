973f15b2515e31ca993093a9eee49bf7509d8a5e
Yujin Jung <devyuu08@gmail.com>
2025-06-07 11:43:16 +0900

    fix: ItineraryDetail 컴포넌트에서 createdAt 포맷 오류 처리

--- src/pages/itinerary/Public.jsx
+++ src/pages/itinerary/Public.jsx
@@ -1,9 +1,5 @@
 import PublicItinerary from "@/components/itinerary/PublicItinerary";
 
 export default function PublicItineraryPage() {
-  return (
-    <div className="max-w-4xl mx-auto pt-10 px-4">
-      <PublicItinerary />
-    </div>
-  );
+  return <PublicItinerary />;
 }
