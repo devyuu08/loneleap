01a79119da4a984eda645a9d3353896f51233a33
Yujin Jung <devyuu08@gmail.com>
2025-06-07 14:24:49 +0900

    feat: 공통 버튼 스타일 및 Tailwind 커스터마이징 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,19 @@ export default {
         heading: ["SpoqaHanSansNeo", "sans-serif"],
         body: ["Pretendard", "sans-serif"],
       },
+      colors: {
+        primary: "#000000",
+        secondary: "#6D8591",
+        danger: "#ef4444",
+      },
+      spacing: {
+        btnX: "1.5rem",
+        btnY: "0.75rem",
+      },
+      borderRadius: {
+        xl: "1rem",
+        full: "9999px",
+      },
     },
   },
   plugins: [],
